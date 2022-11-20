8bb89686ceeec3c79497c76b3bd3c5a89e668b64
Jordan Lambrecht <jordan@pixelbakery.com>
2022-11-20 01:06:32 -0600

    style updates

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <NextSeo
         title='Bergen Johnston'
-        description={`Hi, I'm Jordan Lambrecht, a Professional Something or Another in Lincoln, Nebraska.`}
+        description={`Hi, I'm Bergen Johnston, a Professional Something or Another in Lincoln, Nebraska.`}
         canonical='https://bergenjohnston.com'
         openGraph={{
           url: "https://bergenjohnston.com",
@@ -21,7 +21,7 @@ export default function Home() {
       <Section id='landing' className={""}>
         <div className='h-full flex justify-center lg:flex-row gap-x-24 gap-y-16'>
           <div className='self-center prose prose-stone  font-body'>
-            <h1 className='prose prose-gray text-4xl dark:prose-invert font-semibold opacity-75 tracking-wide'>
+            <h1 className='prose prose-gray text-4xl font-semibold opacity-75 tracking-wide'>
               This is literally a blank page.
             </h1>
             <p className='prose prose-gray prose-lg opacity-75'>
